22d79b488b794d419b2ad200fdd9a0e23e3460a8
wanghongying <wanghongying@didichuxing.com>
2020-03-12 14:14:57 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
   extends: [
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
-    'plugin:vue/recommended'
+    'plugin:vue/recommended',
+    'prettier'
   ],
+  plugins: ['prettier'],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
